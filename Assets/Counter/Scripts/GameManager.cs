4a4bd8f3d0d34f63a26aabdece533e821575d988
Mykola Krasko <mikokrsk@gmail.com>
2023-07-09 22:38:15 +0300

    Add Game Manager
    + Add GameManager script
    + Add Spawn and Restart Button
    = Update func Spawn in SpawnManager

---
+++ Assets/Counter/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Counter/Scripts/SpawnManager.cs
+++ Assets/Counter/Scripts/SpawnManager.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject ball;
-    [SerializeField] float minPosX;
     [SerializeField] float maxPosX;
     [SerializeField] float minPosY;
     [SerializeField] float maxPosY;
-    [SerializeField] float minPosZ;
     [SerializeField] float maxPosZ;
     [SerializeField] int countBalls;
-    private int actualCountBalls;
     // Start is called before the first frame update
     void Start()
     {
-        actualCountBalls = 0;
-        StartCoroutine(SpawnBalls());
+        
     }
 
     // Update is called once per frame
@@ -25,23 +21,12 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
-
-    IEnumerator SpawnBalls()
+    
+    public void SpawnBall()
     {
-        yield return new WaitForSeconds(3f);
-        Vector3 position = new Vector3(GeneratePosition(minPosX,maxPosX),GeneratePosition(minPosY,maxPosY)
-            ,GeneratePosition(minPosZ,maxPosZ));
-        Instantiate(ball,position,ball.transform.rotation);
-        actualCountBalls++;
-        if (actualCountBalls<countBalls)
-        {
-            StartCoroutine(SpawnBalls());
-        }
-        else
-        {
-            StopCoroutine(SpawnBalls());
-        }
-
+        Vector3 position = new Vector3(GeneratePosition(-maxPosX, maxPosX), GeneratePosition(minPosY, maxPosY)
+                    , GeneratePosition(-maxPosZ, maxPosZ));
+        Instantiate(ball, position, ball.transform.rotation);
     }
 
     private float GeneratePosition(float _minPos , float _maxPos)
